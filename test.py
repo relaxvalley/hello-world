603f07d96732f99eb40ce6315524f69ff54ffa17
relaxvalley <73322882+relaxvalley@users.noreply.github.com>
2020-10-24 19:47:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy() 
 app=Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://edkngdec:b1hq_EAX-fAcPJsEA8kyP9Pp32ZPUWkT@kandula.db.elephantsql.com:5432/edkngdec" 
+
+db.init_app(app) 
+
 
 @app.route('/')
 def hello_world():
+  sql_cmd="""select * from users""" 
+  query_data=db.engine.execute(sql_cmd) 
+  print(query_data) 
   return 'Hello World' 
 
 if __name__=='__main__':
   app.run() 
+
